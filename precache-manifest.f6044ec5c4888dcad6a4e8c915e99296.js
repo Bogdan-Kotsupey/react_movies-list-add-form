da5956f1a0c33c03d476e60c4008e9ba46ed0730
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-06-01 14:08:06 +0300

    Updates

---
+++ precache-manifest.f6044ec5c4888dcad6a4e8c915e99296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1e7d1f9c78b785001d77b98834306e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07e3d8fbd0206eb9ed0f",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "5db22c24235ae189e9b6",
+    "url": "./static/css/main.374d36ce.chunk.css"
+  },
+  {
+    "revision": "07e3d8fbd0206eb9ed0f",
+    "url": "./static/js/2.1519192d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1519192d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db22c24235ae189e9b6",
+    "url": "./static/js/main.17df7999.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.f6044ec5c4888dcad6a4e8c915e99296.js"
 );
 
 self.addEventListener('message', (event) => {
